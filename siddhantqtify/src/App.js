031ed088710a2931c6ed9777752374acae6ef6f7
Siddhant Patwa <siddhantpatwa8@gmail.com>
2023-12-31 03:52:04 +0530

    Module-3

--- siddhantqtify/src/App.js
+++ siddhantqtify/src/App.js
@@ -1,18 +1,40 @@
-// import logo from './logo.svg';
-// import './App.css';
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "./components/NavBar/NavBar";
 import Hero from "./components/Hero/Hero";
+import { Outlet } from "react-router-dom";
+import { fetchNewAlbums, fetchSongs, fetchTopAlbums } from "./api/api";
+import { StyledEngineProvider } from "@mui/material";
 
-
-function App() {
+const App = () => {
   const [searchData, setSearchData] = useState();
+  const [data, setData] = useState({});
+
+  const generateData = (key, source) => {
+    source().then((data) => {
+      setData((prevData) => {
+        return {...prevData, [key]: data};
+      })
+    });
+  }
+  
+  useEffect (() => {
+    generateData("topAlbums", fetchTopAlbums);
+    generateData("newAlbums", fetchNewAlbums);
+    generateData("songs", fetchSongs);
+  }, []);
+
+  const { topAlbums = [] , newAlbums = [], songs = [] } = data;
+
   return (
     <>
-      <NavBar searchData={searchData}/>
-      <Hero/>
+      <StyledEngineProvider injectFirst>
+        <NavBar searchData={[...topAlbums, ...newAlbums,]} />
+        <Outlet context={{data: {topAlbums, newAlbums, songs}}} />
+      </StyledEngineProvider>
     </>
   );
 }
+
+
 
 export default App;
--- siddhantqtify/src/components/NavBar/NavBar.jsx
+++ siddhantqtify/src/components/NavBar/NavBar.jsx
@@ -3,13 +3,14 @@ import Logo from "../../components/Logo/Logo.jsx";
 import Search from "../../components/Search/Search.jsx";
 import Button from "../../components/Button/Button.jsx";
 import styles from "./NavBar.module.css";
+import { Link } from 'react-router-dom';
 
 const NavBar = ({ searchData }) => {
   return (
       <nav className={styles.navbar}> 
-        <a href='/'>
+        <Link to='/'>
               <Logo />
-        </a>
+        </Link>
         <Search 
             placeholder = {"Search a song of your choice"}
             searchData={searchData}
